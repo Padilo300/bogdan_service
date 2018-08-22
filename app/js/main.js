4cf8a09ce792a056e9c39c6dd4da74a1b85c53e0
padilo300 <padilo300@gmail.com>
2018-08-22 18:05:35 +0300

    1

--- app/js/main.js
+++ app/js/main.js
@@ -9,4 +9,60 @@ $( document ).ready(function(){
             openNav();
         });
 
+        // Валидация формы
+        $('#formOrder').validate({
+            rules: {
+                url_1: {
+                    required: true,
+                    minlength: 10
+                    }, 
+                count1: "required",
+                delivery:{ required: true},
+                msg: "required"
+            },
+            messages:{
+                url_1:{
+                    required: "поле не заполнено или заполнено не верно",
+                    minlength: "в поле должно быть минимум 10 символов",
+                },
+                count1: "Укажите кол-во товара",
+                delivery: "Выберите способ доставки"
+            }
+        });
+
+        // переключение галочки на урну
+        $('button.check').on('click', function(){
+            var formID = $(this).attr('data-form-id')       ; // Узнаем в наш блок по родительскому id
+            $("#"+formID+" input").addClass('ready')        ; // Переключаем класс на input  
+            $(this).hide()                                  ; // Скрыть галку
+            $(this).next().show()                           ; // Показать корзину
+        });
+
+        // сброс полей и преключение с урны на галочку
+        $('button.trash').on('click', function(){
+            formID = $(this).attr('data-form-id')           ; // Узнаем в наш блок по родительскому id
+            $("#"+formID+" input").toggleClass('ready')     ; // Переключаем класс на input
+            $("#"+formID+" input").val("")                  ; // обнуляем поля
+            $(this).hide()                                  ; // скрываем корзину
+            $(this).prev().show()                           ; // показываем галочу
+        });
+
+        // добвать еще товар
+        $('#addProduct').on('click',function(){
+            var row = $('.orderHide');
+            var a   = 0;
+                a   = $.each(row, function(){});
+                a   = a.length;
+            if(a == 0){
+                $('#addProduct').hide()         ;
+            }
+            $.each(row, function(){
+                $(this).removeClass('hide')         ;
+                $(this).removeClass('orderHide')    ;
+                return false                        ;
+            });
+
+        });
+        
+
 });
